92d0654be70f6be9f8ed8e3fea4947d6d3ecfcc5
Olaleke Sangogade <105587442+olalekeolaleke@users.noreply.github.com>
2022-11-05 17:58:39 +0000

    Updating the Schema file and Jupyter Notebook

--- schema.sql
+++ schema.sql
@@ -1,3 +1,36 @@
+-- Table: public.data_mood
+
+-- DROP TABLE IF EXISTS public.data_mood;
+
+CREATE TABLE IF NOT EXISTS public.data_mood
+(
+    name text COLLATE pg_catalog."default",
+    album text COLLATE pg_catalog."default",
+    artist text COLLATE pg_catalog."default",
+    id text COLLATE pg_catalog."default",
+    release_date text COLLATE pg_catalog."default",
+    popularity bigint,
+    length bigint,
+    danceability double precision,
+    acousticness double precision,
+    energy double precision,
+    instrumentalness double precision,
+    liveness double precision,
+    valence double precision,
+    loudness double precision,
+    speechiness double precision,
+    tempo double precision,
+    key bigint,
+    time_signature bigint,
+    mood text COLLATE pg_catalog."default"
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.data_mood
+    OWNER to postgres;
+
+
 -- Table: public.music_data
 
 -- DROP TABLE IF EXISTS public.music_data;
@@ -27,4 +60,11 @@ TABLESPACE pg_default;
 ALTER TABLE IF EXISTS public.music_data
     OWNER to postgres;
     
+-- Selecting all data from music_data table
+
 SELECT * FROM public.music_data;
+
+-- Selecting all data from data_mood table
+
+SELECT * FROM public.data_mood;
+    
